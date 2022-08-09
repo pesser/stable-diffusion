b076e96fddd80791a620ba0bc37f02be1f8f6945
Gene Kogan <kogan.gene@gmail.com>
2022-08-09 15:22:37 +0000

    nicer slerp

--- generate.py
+++ generate.py
@@ -68,39 +68,18 @@ def make_batch(image, mask, device):
 
 
 def slerp(t, v0, v1, DOT_THRESHOLD=0.9995):
-    c = False
-    if not isinstance(v0,np.ndarray):
-        c = True
-        v0 = v0.detach().cpu().numpy()
-    if not isinstance(v1,np.ndarray):
-        c = True
-        v1 = v1.detach().cpu().numpy()
-    # Copy the vectors to reuse them later
-    v0_copy = np.copy(v0)
-    v1_copy = np.copy(v1)
-    # Normalize the vectors to get the directions and angles
-    v0 = v0 / np.linalg.norm(v0)
-    v1 = v1 / np.linalg.norm(v1)
-    # Dot product with the normalized vectors (can't use np.dot in W)
-    dot = np.sum(v0 * v1)
-    # If absolute value of dot product is almost 1, vectors are ~colineal, so use lerp
+    dot = np.sum(v0 * v1 / (np.linalg.norm(v0) * np.linalg.norm(v1)))
     if np.abs(dot) > DOT_THRESHOLD:
-        return lerp(t, v0_copy, v1_copy)
-    # Calculate initial angle between v0 and v1
-    theta_0 = np.arccos(dot)
-    sin_theta_0 = np.sin(theta_0)
-    # Angle at timestep t
-    theta_t = theta_0 * t
-    sin_theta_t = np.sin(theta_t)
-    # Finish the slerp algorithm
-    s0 = np.sin(theta_0 - theta_t) / sin_theta_0
-    s1 = sin_theta_t / sin_theta_0
-    v2 = s0 * v0_copy + s1 * v1_copy
-    if c:
-        res = torch.from_numpy(v2).to("cuda")
+        v2 = (1 - t) * v0 + t * v1
     else:
-        res = v2
-    return res
+        theta_0 = np.arccos(dot)
+        sin_theta_0 = np.sin(theta_0)
+        theta_t = theta_0 * t
+        sin_theta_t = np.sin(theta_t)
+        s0 = np.sin(theta_0 - theta_t) / sin_theta_0
+        s1 = sin_theta_t / sin_theta_0
+        v2 = s0 * v0 + s1 * v1
+    return v2
 
 
 def run_inpainting(opt, input_image, mask_image, callback=None, update_image_every=1):
